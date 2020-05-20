17e63a34be409c80845111b561817f7372cae927
1 <1@1-PC>
2020-05-20 18:31:31 +0900

    20.05.20 plus

--- src/Java0520/EX04_IciaStuMain.java
+++ src/Java0520/EX04_IciaStuMain.java
@@ -8,13 +8,19 @@ public class EX04_IciaStuMain {
 		
 		Ex04_IciaStu infoAll1 = new Ex04_IciaStu("유제상",27, 
 				"youtube관리하기", "인천시 계양구", "1994-12-04");
+		infoAll1.age();
 		infoAll1.showGroupInfo();
 		Ex04_IciaStu infoAll2 = new Ex04_IciaStu("이은솔", 25,
 				"드라마 시청하기", "인천시 미추홀구", "1996-06-18" );
+		infoAll2.age();
+		infoAll2.showGroupInfo();
 		Ex04_IciaStu infoAll3 = new Ex04_IciaStu("강지수", 27,
 				"youtube 시청", "인천시 서구", "1994-04-27");
-		
-		
+		infoAll3.age();
+		infoAll3.showGroupInfo();
+
+//			  stu3.age(); // 메소드 age를 먼저 호출해야 나이 변함
+//			  stu3.showInfo();
 		
 		
 		
--- src/Java0520/Ex04_IciaStu.java
+++ src/Java0520/Ex04_IciaStu.java
@@ -10,27 +10,45 @@ public class Ex04_IciaStu {
 	// 그사람 정보를 모두 출력하는 메소드 생성
 	// 호출 시 나이를 한살 추가하도록 하는 메소드 생성
 
-	String name;
-	int age;
-	String hobby;
-	String address;
-	String birth;
 
-	Ex04_IciaStu(String name, int age, String hobby, String address, String birth) {
-		this.name = name;
-		this.age = age;
-		this.hobby = hobby;
-		this.address = address;
-		this.birth = birth;
-	}
-	// 메소드
+	// ICIA 교육원에 다니는 JS반 훈련생에 대한 클래스 만들고
+	 // 메인 클래스에서 각자 같은 조원들에 대한
+	 // 객체를 각각 선언해서 출력!
+	 
+	 // 이름, 나이, 취미, 주소, 생년월일
+	 String name;
+	 int age;
+	 String habit;
+	 String address;
+	 String birth;
+	 
+	 
+	 Ex04_IciaStu(String name, int age, String habit,
+	   String address, String birth){
+	  this.name = name;
+	  this.age = age;
+	  this.habit = habit;
+	  this.address = address;
+	  this.birth = birth;
+	  
+	 }
+	 
+	 
+	 void showGroupInfo() {
+	  System.out.println("name : " + name + "\nage : " + 
+	 age + "\nhabit : " + habit + "\naddress : " + address + 
+	 "\nbirth : " + birth + "\n");
+	 }
+	 
+	 int age() {
+	  int result = age--;
+	  return result;
+	 }
+	 
 
-	void showGroupInfo() {
-		System.out.println("\n name : " + name + "\n age : " + age + "\n hobby" + hobby + "\n address" + address
-				+ "\n birth" + birth);
-	}
+	 // 그 사람 정보를 모두 출력하는 메소드 생성
+	 
+	 // 호출시 나이를 한살 추가하도록 하는 메소드 생성
+	 // void 말고 다른 데이터타입 사용!
 
-	int minusAgeInfo() {
-		System.out.println("\n age : " + age);
-	}
 }
